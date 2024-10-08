4f00844f124645992f0e6a5fab02f19222c2eae9
TeslaZY <TeslaZY@outlook.com>
2024-10-08 15:07:51 +0800

    modified:   sqlserver/init.sql

--- sqlserver/init.sql
+++ sqlserver/init.sql
@@ -19,56 +19,9 @@ SELECT * FROM sys.database_permissions WHERE grantee_principal_id = (SELECT prin
 
 -- DELETE FROM demo.dbo.A8_Store_List; 
 
+select  count(*) from A8_Daily_Sales_Details adsd ;
 
--- demo.dbo.[A8_2022_Sales_Targets_for_Each_Store.csv] definition
+use master;
 
--- Drop table
-
--- DROP TABLE demo.dbo.[A8_2022_Sales_Targets_for_Each_Store.csv];
-
-CREATE TABLE demo.dbo.[A8_2022_Sales_Targets_for_Each_Store.csv] (
-	[Store Number] int NULL,
-	[ Sales Target] int NULL
-);
-
-
--- demo.dbo.[A8_2022_Store_Foot_Traffic.csv] definition
-
--- Drop table
-
--- DROP TABLE demo.dbo.[A8_2022_Store_Foot_Traffic.csv];
-
-CREATE TABLE demo.dbo.[A8_2022_Store_Foot_Traffic.csv] (
-	[Date] varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[Store Number] int NULL,
-	[Foot Traffic] int NULL,
-	[Number of Visitors] int NULL
-);
-
-
--- demo.dbo.A8_Daily_Sales_Details definition
-
--- Drop table
-
--- DROP TABLE demo.dbo.A8_Daily_Sales_Details;
-
-CREATE TABLE demo.dbo.A8_Daily_Sales_Details (
-	[Date] varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[Product Category] varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[Store Number] int NULL,
-	[Daily Sales] int NULL
-);
-
-
--- demo.dbo.A8_Store_List definition
-
--- Drop table
-
--- DROP TABLE demo.dbo.A8_Store_List;
-
-CREATE TABLE demo.dbo.A8_Store_List (
-	[Store Number] int NULL,
-	[Store Name] nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	City nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	Province nvarchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
-);
+CREATE USER marmot FOR LOGIN marmot;
+GRANT SELECT, INSERT, UPDATE, ALTER ON SCHEMA::dbo TO marmot;
